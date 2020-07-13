54f4d01ea9caaa19585c906a4eb230f8146f4d8c
sfarnsworth02 <farnsworth.sarah02@gmail.com>
2020-07-12 18:34:51 -0600

    able to target specific activity

--- src/pages/activity/Activity.Form.js
+++ src/pages/activity/Activity.Form.js
@@ -8,92 +8,40 @@ export default class ActivityForm extends React.Component
         super(props);
         this.state = 
         {
-            action: props.action,
+            title: props.title,
+            editing: props.editing,  
+            isVisible: null,
             activity: props.activity,
-            description: props.description,
-            duration: props.duration,
-            message: '',
         }
     }
-
+    
     handleChange = (e) =>
     {
         e.preventDefault();
-        let fieldName = e.target.getAttribute('name');
-        const activityObj = {};
-        activityObj[fieldName] = e.target.value;
-        this.setState(activityObj);
+        this.setState({[e.target.name]: e.target.value });
     }
 
-    handleNewActivity = (e) =>
+    update = (e) =>
     {
         e.preventDefault();
-        let path = Config.basePath;
-        let body = ['activity', 'description', 'duration'];
-        let dataBody = {};
-        for (let i=0; i<body.length; i++)
-        {
-            const field = body[i];
-            dataBody[field] = this.state[field];
-        };
-        console.log('dataBody: ', dataBody);
-        let fetchOptions = {
-            headers: { 'Content-Type':'application/json' },
-            method: 'POST',
-            mode: 'cors',
-            body: JSON.stringify(dataBody)
-        };
-        let activityUrl = `${path}/activity`;
-        fetch(activityUrl, fetchOptions)
-        .then((response) => {
-            if(response.status !== 200)
-            {
-                return ('Fetch response unsuccessful: ', response)
-            }
-            return response.json();
-        })
-        .then((result) => {
-            if(result.status !== 200)
-            {
-                return ('Fetch result unsuccessful: ', result);
-            }
-            this.setState({message:`New activity created:  ${result}`})
-            return(console.log('Fetch result a success! ', result));
-        })
-        .catch((err) => {
-            if(err)
-            {
-                console.log('Catching fetch error: ', err);
-            }
-        });
+        console.log('lets update some shit');
     }
 
     render()
     {
+        console.log('render form props: ', this.state)
         return(
             <div>
-                <h3>{this.state.action}:</h3>
+                <h3>{this.state.title}:</h3>
+                <span>{this.state.message}</span>
                 <form className='create-habit-form' onSubmit={this.handleNewActivity}>
                     <ul>
-                        <label htmlFor='activity'>New Activity</label>
                         <input 
                             type='text'
                             name='activity'
                             value={this.state.activity}
                             onChange={this.handleChange} />
-                        <label htmlFor='description'>Description</label>
-                        <input 
-                            type='text'
-                            name='description'
-                            value={this.state.description}
-                            onChange={this.handleChange} />
-                        <label htmlFor='duration'>Duration</label>
-                        <input 
-                            type='number'
-                            name='duration'
-                            value={this.state.duration}
-                            onChange={this.handleChange} />
-                        <input type='submit' />
+                        <input type='submit' value='Save' />
                     </ul>
                 </form>
             </div>
--- src/pages/activity/Activity.List.js
+++ src/pages/activity/Activity.List.js
@@ -8,8 +8,9 @@ function ActivityItem (props)
 {
     return(
         <li>
+            <span>{props.index}-</span>
             <span> {props.activity} </span>
-            <button onClick={props.editActivity}>Edit</button>
+            <button onClick={props.editActivity} index={props.index}>Edit</button>
             <button onClick={props.deleteActivity} item={props.id}>Delete</button>
         </li>
     )
@@ -21,11 +22,10 @@ export default class List extends React.Component
     {
         super(props);
         this.state = {
+            editing: null,
             activities: [],
-            action:'Create',
-            activity: '',
-            description:'',
-            duration:'',
+            activity:'',
+            index:'',
         }
     }
     getActivityList = async () =>
@@ -59,7 +59,18 @@ export default class List extends React.Component
     }
     editActivity = (e) =>
     {
-
+        e.preventDefault();
+        const i = e.target.getAttribute('index');
+        const index = this.state.activities[i].props.index;
+        const activity = this.state.activities[i].props.activity;
+        this.setState({
+            editing:'Edit',
+            index: index,
+            activity: activity,
+        });
+        console.log('editActivity checking state: ', this.state.index);
+        console.log('editActivity e.target.activty is: ', activity);
+        console.log('editActivity editing? ', this.state.editing)
     }
 
     deleteActivity = (e) =>
@@ -96,6 +107,48 @@ export default class List extends React.Component
         })
     }
 
+    handleNewActivity = (e) =>
+    {
+        e.preventDefault();
+        let path = Config.basePath;
+        let body = ['activity', 'description', 'duration'];
+        let dataBody = {};
+        for (let i=0; i<body.length; i++)
+        {
+            const field = body[i];
+            dataBody[field] = this.state[field];
+        };
+        let fetchOptions = {
+            headers: { 'Content-Type':'application/json' },
+            method: 'POST',
+            mode: 'cors',
+            body: JSON.stringify(dataBody)
+        };
+        let activityUrl = `${path}/activity`;
+        fetch(activityUrl, fetchOptions)
+        .then((response) => {
+            if(response.status !== 200)
+            {
+                return ('Fetch response unsuccessful: ', response)
+            }
+            return response.json();
+        })
+        .then((result) => {
+            if(result.status !== 200)
+            {
+                return ('Fetch result unsuccessful: ', result);
+            }
+            this.setState({message:`New activity created:  ${result}`})
+            return(console.log('Fetch result a success! ', result));
+        })
+        .catch((err) => {
+            if(err)
+            {
+                console.log('Catching fetch error: ', err);
+            }
+        });
+    }
+
     componentDidMount()
     {
         this.getActivityList();
@@ -107,13 +160,12 @@ export default class List extends React.Component
             <div>
                 {/* <ActvContext.Provider value={this.state}> */}
                     <Form
-                        action={this.state.action === "Create"? "Create a new Activity": "Update activity"}
+                        title={this.state.editing !== "Edit" ? "Create a new Activity": "Update activity"}
                         activity={this.state.activity}
-                        description={this.state.description}
-                        duration={this.state.duration}
-                        getList={this.getActivityList} />
+                        onUpdate={this.editActivity} />
+                        <span>current activity is: {this.state.activity} {this.state.description}</span>
                     <ul>
-                        {this.state.activities}
+                        <span>{this.state.activities}</span>
                     </ul>  
                 {/* </ActvContext.Provider> */}
             </div>
